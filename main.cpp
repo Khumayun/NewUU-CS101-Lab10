40572d01b74076d0c27f897f387faea9491e4fe8
k.aminaddinov <k.aminaddinov@newuu.uz>
2024-04-28 13:14:07 +0500

    added problems.h with functions headers and main.cpp with sample code to run.

--- main.cpp
+++ main.cpp
@@ -1,170 +1,185 @@
 #include <iostream>
-#include <cmath>
-
+#include "problems.h"
 
 using namespace std;
 
-void displayPattern(int n) {
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
 
-}
-
-int getType(int n){
-    return 1;
-}
-
-
-double problem3(){
-    return 1;
-}
-
-// P4
-
-
-int countDigitOccurrences(int num, int digit) {
-    return 1;
-}
-void findDigitFrequency(int number) {
-
-}
-
-// p5
-
-void digitToWord(int n){
-
-}
-void numberToWords(int n){
-
-}
-
-
-//p6
-
-//p7
-
-void solveEquation(double a, double b, double c, double d, double e, double f, double& x, double& y, bool& isSolvable) {
-
-}
-
-//p8
-void inputCoordinates(double& x1, double& y1, double& x2, double& y2,
-                      double& x3, double& y3, double& x4, double& y4) {
-
-}
-
-// Function to check if vectors AB and CD are orthogonal using dot product
-bool areVectorsOrthogonal(double x1, double y1, double x2, double y2,
-                          double x3, double y3, double x4, double y4) {
-    return 1;
-}
-
-// p9
-int octal2Dec(const int& octalNumber) {
-    return 1;
-}
-
-
-void revChar(char& c) {
-
-}
-
-double avgOfEvenPlaces(int n) {
-    return 1;
-}
-
-//p12
-
-// Function to approximate the square root of a number
-double sqrt(double n) {
-    return 1;
-}
-
-//p13
-
-int enc(char a, int k);
-
-//p14
-
-int enc(char a, int k){
-    return 1;
-}
-
-// p15
-
-bool chkkaprekar(int n) {
-    return 1;
-}
-
-//p16
-
-long long int numReverse(long long int number) {
-    return 1;
-}
-bool is_Palindrome(long long int num) {
-    return 1;
-}
-bool isLychrel(int num, const int iterCount = 500) {
-    return 1;
-}
-
-// p17
-
-// Function to calculate the number of digits in a number
-int countDigits(int num) {
-    return 1;
-}
-
-// Function to check if a number is a Narcissistic number
-bool isNarcissistic(int num) {
-    return 1;
-}
-
-// Function to find and display the first N Narcissistic numbers
-void findNarcissisticNumbers(int N) {
-
-}
-
-// p18
-
-double year_to_century(int y) {
-    return 1;
-}
-
-// p19
-
-
-int timeToMinutes(int hours, int minutes) {
-    return 1;
-}
-
-// Function to calculate the absolute difference in minutes between two times
-int calculateTimeDifference(int hours1, int minutes1, int hours2, int minutes2) {
-    return 1;
-}
-
-// p20
-
-// Function to check if a year is a leap year
-bool isLeapYear(int year) {
-    return 1;
-}
-
-// Function to calculate the number of days in a month
-int daysInMonth(int month, int year) {
-    return 1;
-}
-
-// Function to calculate the total number of days from the beginning of the year to a given date
-
-
-// Function to calculate the difference in days between two dates
-int daysBetweenDates(int day1, int month1, int year1, int day2, int month2, int year2) {
-    return 1;
+    cout << endl;
 }
 
 
 int main() {
 
     // TEST YOUR FUNCTIONS IN HERE
+
+    // p1
+    int result1;
+    double result2;
+    result1 = add(2, 3);   // calling with int parameters
+    cout << result1 << endl;
+
+    result2 = add(2.2, 3.3);// calling with double parameters
+    cout << result2 << endl;
+
+    // p2
+
+    int x = 5, y = 10;
+    cout << "Before swapping: x = " << x << ", y = " << y << endl;
+
+    // Call the swap function template
+    Myswap(x, y);
+
+    cout << "After swapping: x = " << x << ", y = " << y << endl;
+
+    // Test the swap function template with double values
+    double a = 3.14, b = 2.718;
+    cout << "Before swapping: a = " << a << ", b = " << b << endl;
+
+    // Call the swap function template
+    Myswap(a, b);
+
+    cout << "After swapping: a = " << a << ", b = " << b << endl;
+
+    //p4
+
+    int size;
+    cin>>size;
+    int arr[size];
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+    double result=getAverage(arr, size);
+    cout<<result<<endl;
+
+    // p5
+
+    int n;
+    cin>>n;
+    int arr5[n];
+    for(int i=0;i<n;i++){
+        cin>>arr5[i];
+    }
+    minimum(arr5, n);
+
+    // p6
+
+    const int arraySize = 15;
+    double numbers[arraySize];
+
+    cout << "Enter 15 numbers:" << endl;
+
+    for (int i = 0; i < arraySize; ++i) {
+        cout << "Enter number " << (i + 1) << ": ";
+        cin >> numbers[i];
+    }
+
+    int largestIndex = indexOfLargestElement(numbers, arraySize);
+
+    if (largestIndex != -1) {
+        cout << "Index of the largest element: " << largestIndex << endl;
+    }
+
+    // p7
+
+    const int maxSize = 100; // You can adjust this based on your requirements
+    int list1[maxSize];
+    int list2[maxSize];
+    int size7;
+
+    cout << "Enter the size of the lists: ";
+    cin >> size7;
+
+    if (size7 <= 0 || size7 > maxSize) {
+        cerr << "Error: Invalid size." << endl;
+        return 1;
+    }
+
+    cout << "Enter the elements of list1:" << endl;
+    for (int i = 0; i < size7; ++i) {
+        cout << "Enter element " << (i + 1) << ": ";
+        cin >> list1[i];
+    }
+
+    cout << "Enter the elements of list2:" << endl;
+    for (int i = 0; i < size7; ++i) {
+        cout << "Enter element " << (i + 1) << ": ";
+        cin >> list2[i];
+    }
+
+    bool areIdentical = strictlyEqual(list1, list2, size7);
+
+    if (areIdentical) {
+        cout << "The lists are strictly identical." << endl;
+    } else {
+        cout << "The lists are not strictly identical." << endl;
+    }
+
+    // p8
+    // p9
+    // p10
+    // p11
+    // p12
+    // p13
+    int num;
+    cin>>num;
+    cout<<sum(num)<<endl;
+    // p14
+
+    cin>>num;
+    cout<<factorial(num)<<endl;
+
+    // p15
+    int pow;
+    cin>>num>>pow;
+    cout<<power(num,pow)<<endl;
+
+    // p16
+
+    int arr16[] = {1, 2, 3, 4, 5, 6};
+
+    int n16 = sizeof(arr) / sizeof(arr[0]);
+
+    // To print original array
+    printArray(arr16, n16);
+
+    // Function calling
+    revereseArray(arr16, 0, n16-1);
+
+    cout << "Reversed array is" << endl;
+
+    // To print the Reversed array
+    printArray(arr16, n16);
+
+    // p17
+    // To print original array
+    printArray(arr, 6);
+
+    // Function calling
+    revereseArrayREC(arr, 0, 5);
+
+    cout << "Reversed array is" << endl;
+
+    // To print the Reversed array
+    printArray(arr, 6);
+
+    // P18
+    cout<<series(5)<<endl;
+
+    // P19
+    cout<<series2(5)<<endl;
+
+    // P20
+    std::cout << "Input a number: ";
+    std::cin >> n;
+
+    // Calculate the sum of digits using recursion
+    int sum_digits = sumOfDigits(n);
+
+    std::cout << "Sum of digits of " << n << " is: " << sum_digits << std::endl;
 
 
     return 0;
---
+++ problems.h
@@ -0,0 +1,79 @@
+// P1
+template <typename T>
+T add(T num1, T num2) {
+    return 0;
+}
+
+// P2
+template <typename T>
+void Myswap(T& a, T& b) {
+
+}
+
+// P3
+
+// P4
+double getAverage(int arr[], int size) {
+    return 0;
+}
+
+// P5
+
+int minimum(int a[],int size){
+    return 0;
+}
+
+// P6
+int indexOfLargestElement(double array[], int size) {
+    return 0;
+}
+
+// P7
+
+bool strictlyEqual(const int list1[], const int list2[], int size) {
+    return true; // All corresponding elements are equal
+}
+// P8
+// P9
+// P10
+// P11
+// P12
+// P13
+int sum(int n){
+    return 0;
+}
+// P14
+
+int factorial(int n){
+    return 0;
+}
+
+// P15
+int power(int n,int p){
+    return 0;
+}
+
+// P16
+void revereseArray(int arr[], int start, int end)
+{
+}
+
+// P17
+void revereseArrayREC(int arr[], int start, int end)
+{
+}
+
+// P18
+double series(int n){
+    return 0;
+}
+
+// P19
+double series2(int n){
+    return 0;
+}
+
+// P20
+int sumOfDigits(int number) {
+    return 0;
+}
